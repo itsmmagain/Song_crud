c24cf755d1cf752d430b94ea5bf7aa6cad914f89
itsmmagain <mmtsalman009@gmail.com>
2022-10-28 10:47:57 +0100

    week 5 django task

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,31 @@
+from email.policy import default
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Artiste(models.Model):
+    First_name = models.CharField(max_length=200)
+    Last_name = models.CharField(max_length=200)
+    Age = models.IntegerField()
+
+
+    def __str__(self):
+        return self.First_name
+
+class Song(models.Model):
+    Title = models.CharField(max_length=200)
+    Release_date = models.DateField(default=datetime.today)
+    Likes = models.IntegerField()
+    Artiste_id = models.ForeignKey(Artiste, on_delete=models.PROTECT)
+
+
+    def __str__(self):
+        return self.Title
+    
+class Lyric(models.Model):
+    Lyric = models.CharField(max_length=40)
+    Content = models.TextField()
+    Song_id = models.ForeignKey(Song, on_delete=models.PROTECT)
+
+
+    def __str__(self):
+        return self.Lyric
